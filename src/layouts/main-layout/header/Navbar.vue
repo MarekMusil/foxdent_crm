bd350287431846d555acddcbc633f5e97d15c8c5
MarekMusil <musil.marek.m@gmail.com>
2024-08-30 12:12:16 +0200

    refactor: Update Navbar.vue to add link to external website

--- src/layouts/main-layout/header/Navbar.vue
+++ src/layouts/main-layout/header/Navbar.vue
@@ -3,6 +3,29 @@
   <div class="app-navbar flex-shrink-0">
     <!--begin::Theme mode-->
     <!--begin::User menu-->
+  
+    <div class="app-navbar-item ms-1 ms-md-4" id="kt_header_web_link_toggle">
+      <!--begin::Menu wrapper-->
+      <div
+        class="cursor-pointer symbol symbol-35px"
+        data-kt-menu-trigger="{default: 'click', lg: 'hover'}"
+        data-kt-menu-attach="parent"
+        data-kt-menu-placement="bottom-end"
+      >
+      
+        <a href="https://foxdentweb.davmosoft.cz/" target="_blank" title="Odkaz na web">
+          <i class="ki-duotone  ki-icon fs-1">
+            <span class="path1"></span>
+            <span class="path2"></span>
+            <span class="path3"></span>
+          </i>
+        </a>
+      </div>
+
+
+      <!--end::Menu wrapper-->
+    </div>
+    
     <div class="app-navbar-item ms-1 ms-md-4" id="kt_header_user_menu_toggle">
       <!--begin::Menu wrapper-->
       <div
@@ -11,11 +34,13 @@
         data-kt-menu-attach="parent"
         data-kt-menu-placement="bottom-end"
       >
-        <i class="ki-duotone ki-user fs-1">
+      
+      <i class="ki-duotone ki-user fs-1">
           <span class="path1"></span>
           <span class="path2"></span>
         </i>
       </div>
+
       <KTUserMenu />
       <!--end::Menu wrapper-->
     </div>
